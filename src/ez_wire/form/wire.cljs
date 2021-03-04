a4f2f60fca49b99dad19ebb8227b51ec8d8bba08
Emil Bengtsson <emil@emil0r.com>
2021-03-04 21:39:41 +0100

    Rename form-map to form in wiring

--- src/ez_wire/form/wire.cljs
+++ src/ez_wire/form/wire.cljs
@@ -9,7 +9,7 @@
 (defn- ->kw [name k]
   (keyword (str "$" (subs (str name) 1) "." (clojure.core/name k))))
 
-(defn- assemble-body [{:keys [wiring]} {:keys [fields] :as form-map} content]
+(defn- assemble-body [{:keys [wiring]} {:keys [fields] :as form} content]
   (let [default-map (if content
                       {:$content [content]}
                       {})]
@@ -17,28 +17,28 @@
                                    (merge out
                                           {(->kw name :wrapper) wiring/unwrapper
                                            (->kw name :key)     {:key (str "ui-wire-form-wire" (:id field))}
-                                           (->kw name :label)   (common/render-label field form-map)
-                                           (->kw name :field)   (common/render-field field form-map)
-                                           (->kw name :errors)  (common/render-error-element field form-map)
-                                           (->kw name :text)    (common/render-text field form-map)
-                                           (->kw name :help)    (common/render-help field form-map)}))
+                                           (->kw name :label)   (common/render-label field form)
+                                           (->kw name :field)   (common/render-field field form)
+                                           (->kw name :errors)  (common/render-error-element field form)
+                                           (->kw name :text)    (common/render-text field form)
+                                           (->kw name :help)    (common/render-help field form)}))
                                  default-map fields))))
 
 (defn as-wire [{:keys [wiring/element]
                 :or {element :div}
                 :as params}
-               {:keys [id form-key] :as form-map} & [content]]
-  (let [body (assemble-body params form-map content)]
+               {:keys [id form-key] :as form} & [content]]
+  (let [body (assemble-body params form content)]
     (r/create-class
      {:display-name "as-wire"
 
       :component-will-unmount
       (fn [this]
-        (when (util/select-option :form/automatic-cleanup? form-map params)
+        (when (util/select-option :form/automatic-cleanup? form params)
           (rf/dispatch [:ez-wire.form/cleanup id])))
 
       :reagent-render
-      (fn [params form-map & [content]]
+      (fn [params form & [content]]
         (let [{:keys [style
                       class]
                :or {style {}
