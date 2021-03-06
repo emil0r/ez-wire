aff18f52c3c6801da3846118082f612c8c408115
Emil Bengtsson <emil@emil0r.com>
2021-03-07 00:28:12 +0100

    Cleanup table implementation
    Played around with hiding the elements via CSS, but had to abandon the
    idea because of as-wire.

--- src/ez_wire/form/table.cljs
+++ src/ez_wire/form/table.cljs
@@ -8,26 +8,21 @@
 
 (defn- table-row [{:keys [active?] :as field} {{label? :label?} :options :as form}]
   (when @active?
-    (let [prop nil #_
-          (when-not @active?
-            {:style {:display "none"}})]
-      (if (false? label?)
-        [:tr (merge {:key (str "tr-" (:id field))}
-                    prop)
-         [:td
-          (common/render-field field form)
-          (common/render-error-element field form)
-          (common/render-text field form)
-          (common/render-help field form)]]
-        [:tr (merge {:key (str "tr-" (:id field))}
-                    prop)
-         [:td
-          (common/render-label field form)]
-         [:td
-          (common/render-field (dissoc field :label) form)
-          (common/render-error-element field form)
-          (common/render-text field form)
-          (common/render-help field form)]]))))
+    (if (false? label?)
+      [:tr {:key (str "tr-" (:id field))}
+       [:td
+        (common/render-field field form)
+        (common/render-error-element field form)
+        (common/render-text field form)
+        (common/render-help field form)]]
+      [:tr {:key (str "tr-" (:id field))}
+       [:td
+        (common/render-label field form)]
+       [:td
+        (common/render-field (dissoc field :label) form)
+        (common/render-error-element field form)
+        (common/render-text field form)
+        (common/render-help field form)]])))
 
 (defn as-table [params {:keys [id form-key] :as form} & [content]]
   (r/create-class
@@ -46,6 +41,7 @@
         [:table {:key (util/slug "form-table" (str @form-key))
                  :style style
                  :class class}
-         [:tbody [common/get-body table-row params form]
+         [:tbody
+          [common/get-body table-row params form]
           (if content
             [content])]]))}))
